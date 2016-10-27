81c5700dad02f6c681138fdc6cc62cb5b4db53d3
kiubis <kiubis96@gmail.com>
2016-10-27 16:03:14 +0200

    Add files via upload

--- greenhouse.ino
+++ greenhouse.ino
@@ -4,7 +4,7 @@
 LiquidCrystal lcd(53,52,51,50,49,48);
 
 const byte rows = 4; //four rows
-const byte cols = 4; //three columns
+const byte cols = 4; //four columns
 char keys[rows][cols] = {
   {'1','2','3','A'},
   {'4','5','6','B'},
@@ -15,16 +15,55 @@ byte rowPins[rows] = {22,24,26,28}; //connect to the row pinouts of the keypad
 byte colPins[cols] = {23,25,27,29}; //connect to the column pinouts of the keypad
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, rows, cols );
 
-void setup() {
-  // put your setup code here, to run once:
-lcd.begin(16,2);
-lcd.print("Hello world!");
-Serial.begin(9600);
+int cykl=1;
 
+void setup() {
+lcd.begin(16,2);
+lcd.clear();
+Serial.begin(9600);
+Welcome();
+Menu1();
+MenuLogin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
-
+lcd.setCursor(12,0);
+lcd.print(analogRead(A0)*0.0049);
+delay(100);
+lcd.setCursor(12,1);
+lcd.print(analogRead(A1)*0.0049);
+delay(100);
+cykl++;
+if(cykl==25){
+  cykl=1;
+  MenuLogin();
+  }
 }
+
+void Welcome(){
+lcd.clear();
+lcd.setCursor(5,0);
+lcd.print("Welcome");
+lcd.setCursor(1,1);
+lcd.print("please wait...");
+delay(1500);
+lcd.clear();
+}
+
+void Menu1(){
+lcd.clear();
+lcd.print("LightVol: ");
+lcd.setCursor(0,1);
+lcd.print("ContrastVol: ");
+ }
+
+void MenuLogin(){
+lcd.clear();
+lcd.print("Press D for menu");
+lcd.setCursor(5,1);
+lcd.print("access");
+delay(2000);
+Menu1();
+loop();
+}
+
